960bbd8462d705046ef6829d598e1e7fb0f5281b
Teemu Kerkola <tokerk@utu.fi>
2025-02-05 16:22:38 +0200

    feat: add dialog content for play again and quit

--- src/app/game/[gameId]/(components)/game-finished-dialog.tsx
+++ src/app/game/[gameId]/(components)/game-finished-dialog.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import Link from "next/link";
+
 import { GameEndReason, GameState } from "@prisma/client";
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import {
+   Dialog,
+   DialogContent,
+   DialogFooter,
+   DialogHeader,
+   DialogTitle,
+} from "@/components/ui/dialog";
 import { usePlayer } from "@/hooks/use-player";
 import { Game } from "@/utils/game-db";
 
@@ -13,16 +22,18 @@ export function GameFinishedDialog({ initialGame }: { initialGame: Game }) {
    const { game } = useGame(initialGame);
    const winnerId = useGameStore((s) => s.winnerId);
    const { playerId, hasHydrated } = usePlayer();
+
    const winnerName = winnerId === game.player1Id ? game.player1Name : game.player2Name;
+   const opponentName = playerId === game.player1Id ? game.player2Name : game.player2Name;
    const isOpen = game.state === GameState.FINISHED;
 
    if (!hasHydrated) return null;
 
    return (
       <Dialog open={isOpen}>
-         <DialogContent hideCloseButton>
+         <DialogContent hideCloseButton className="text-center">
             <DialogHeader>
-               <DialogTitle>
+               <DialogTitle className="text-center">
                   {game.gameEndReason === GameEndReason.TIE
                      ? "It's a Tie!"
                      : winnerId === playerId
@@ -30,13 +41,24 @@ export function GameFinishedDialog({ initialGame }: { initialGame: Game }) {
                        : "You Lost!"}
                </DialogTitle>
             </DialogHeader>
-            <div className="text-center">
+
+            <div>
                {game.gameEndReason === GameEndReason.TIE ? (
                   <p>Both players fought valiantly, but it ended in a tie!</p>
                ) : (
                   <p>{winnerName} is the lucky winner!</p>
                )}
+
+               <p>{opponentName} wants to play again!</p>
             </div>
+            <DialogFooter>
+               <div className="flex w-full flex-wrap gap-2">
+                  <Button className="w-full">Play again?</Button>
+                  <Button variant="outline" asChild className="w-full">
+                     <Link href="/">Quit</Link>
+                  </Button>
+               </div>
+            </DialogFooter>
          </DialogContent>
       </Dialog>
    );
