f5a3de28d00501f3724198284793214ae35b3b3e
Teemu Kerkola <tokerk@utu.fi>
2025-02-06 13:37:53 +0200

    feat: add cronjob configuration

--- src/app/api/clean-up-db/route.ts
+++ src/app/api/clean-up-db/route.ts
@@ -2,7 +2,10 @@ import { NextResponse } from "next/server";
 
 import { db } from "@/utils/db";
 
-export async function POST() {
+export async function POST(req: Request) {
+   if (req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+   }
    const result = await db.game.cleanUpDatabase();
    if (result.success) {
       return NextResponse.json({ message: result.message }, { status: 200 });
